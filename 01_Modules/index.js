e2a98eb3ca149b23ff779817fcd87d4b9c2eac67
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-09-05 19:45:41 +0530

    Read/Write delete make and remove directory using File Systems(fs)

--- 01_Modules/index.js
+++ 01_Modules/index.js
@@ -1,5 +1,14 @@
-const fs = require("fs");
+const fs = require("node:fs");
 
 const content = fs.readFileSync("notes.txt", "utf-8");
+// console.log(content);
 
-console.log(content);
+// fs.writeFileSync("copy.txt", "I am writing my name Shivansh in this", "utf-8");
+// fs.writeFileSync("copy.txt", content, "utf-8");
+
+fs.appendFileSync("copy.txt", "Hello NodeJS", "utf-8");
+
+fs.mkdirSync("games", { recursive: true });
+fs.rmdirSync("games");
+
+fs.unlinkSync("copy.txt");
